96ef3ec077c0b1f2fab0d7ac55ecc52f83a7dcd6
Sune <sunenilausen@gmail.com>
2016-05-03 14:55:18 +0200

    more

--- ValgfagProjektApp/main.js
+++ ValgfagProjektApp/main.js
@@ -1,3 +1,17 @@
-var html = "";
+function myFunction(){
 
-document.getElementById("myOutput").innerHTML = html;
+  
+  var x = $('#sel1').val();
+  var y = $('#sel2').val();
+  
+  
+  
+  var html = "";
+  html += "<tr class='success'>" ;
+  html += "<td>" + x[0] + "</td>";
+  html += "<td>" + y[0] + "</td>";
+  html += "<td>" + "Øvelse Resultat" + "</td>";
+  html += "</tr>";
+
+  document.getElementById("myOutput").innerHTML = html;
+}
