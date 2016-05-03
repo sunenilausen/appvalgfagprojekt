2c7895d88e55f37d26748e0aa7bcc5dc4ecc9673
Sune <sunenilausen@gmail.com>
2016-05-03 15:53:07 +0200

    finished

--- ValgfagProjektApp/main.js
+++ ValgfagProjektApp/main.js
@@ -1,23 +1,32 @@
-function myFunction(){
+var Reb = new Array();
+Reb["Biceps"] = "https://www.youtube.com/watch?v=Pu-_Eqsbblk";
+Reb["Ryg"] = "https://www.youtube.com/watch?v=Eq_ArlGoZac";
+Reb["Triceps"] = "https://www.youtube.com/watch?v=KHfkC4Atxh0";
 
-  var x = $('#sel1').val();
-  var y = $('#sel2').val();
-  
-  
-  var html = "";
-  
-  for(i=0;i<x.length;i++)
-  {
-    for(j=0;j<y.length;j++)
-    {
-      html += "<tr class='success'>" ;
-      html += "<td>" + x[i] + "</td>";
-      html += "<td>" + y[j] + "</td>";
-      html += "<td>" + "Øvelse Resultat" + "</td>";
-      html += "</tr>";
+var Håndvægt = new Array();
+Håndvægt["Biceps"] = "https://www.youtube.com/watch?v=taoKFgbH0dQ";
+Håndvægt["Ryg"] = "https://www.youtube.com/watch?v=iMj2hTaxqu4";
+Håndvægt["Triceps"] = "https://www.youtube.com/watch?v=c0FYn7y-TG0";
+
+var Værktøjer = new Array();
+Værktøjer["Reb"] = Reb;
+Værktøjer["Håndvægt"] = Håndvægt;
+
+function myFunction() {
+    var x = $('#sel1').val();
+    var y = $('#sel2').val();
+    
+    var z = 0;
+    var html = "";
+    for (j = 0; j < y.length ;j++) {
+      for (i = 0; i < x.length ;i++) {
+        html += '<tr class="success">';
+        html += '<td>' + x[i] + '</td>';
+        html += '<td>' + y[j] + '</td>';
+        z = Værktøjer[x[i]];
+        html += '<td>' + '<a href=' + z[y[j]] + '>Øvelse Link</a>' + '</td>';
+        html += '</tr>';
+      }
     }
-  }
-  
-  document.getElementById("myOutput").innerHTML = html;
- 
+    document.getElementById("myOutput").innerHTML = html;
 }
